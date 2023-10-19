6e16537e825763f71e77bae01d9c5638128890a5
Łukasz Saletnik <luksaletnik@gmail.com>
2023-10-19 17:25:56 +0200

    Maked screenshots folder

--- screenshot_function.py
+++ screenshot_function.py
@@ -3,5 +3,5 @@ import datetime
 def make_screenshot(driver):
     today = datetime.datetime.today()
     short_date = today.strftime('_stamp%H%M%S')
-    screen_name = 'screen' + short_date + '.png'
+    screen_name = 'C:\\Users\\luksa\\PycharmProjects\\selenium_2023.09.24\\saucedemo_screenshot\\' + 'screen' + short_date + '.png'
     driver.get_screenshot_as_file(screen_name)
